797b09a0f6082a158e774509b674c959d9e04330
ananyaabburu <ananya.abburu@gmail.com>
2026-06-30 19:41:03 +0000

    week 5 added chatbot protype+stylistic

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
-function showMessage() {
-    alert("Welcome to the Astrophysics Learning Platform!");
+const API_KEY = "APIKEYHERE";
+
+async function askGemini() {
+    const question = document.getElementById("userQuestion").value;
+    const responseBox = document.getElementById("response");
+
+    responseBox.innerText = "Thinking...";
+
+const res = await fetch(
+  `https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=${API_KEY}`,
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      contents: [{
+        parts: [{
+          text: question
+        }]
+      }]
+    })
+  }
+);
+
+    const data = await res.json();
+
+    console.log("FULL RESPONSE:", data);
+
+responseBox.innerText =
+    JSON.stringify(data, null, 2);
 }
+
+window.askGemini = askGemini;
